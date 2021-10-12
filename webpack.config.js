eed1cd33dd94a247383d998c9288797ee1964af5
achimcc <achim@parity.io>
2021-10-12 10:45:23 +0200

    webpack hash -> fullhash

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     app: "./src/index.tsx",
   },
   output: {
-    filename: "bundle.[hash].js",
+    filename: "bundle.[fullhash].js",
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
